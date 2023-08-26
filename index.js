c7a30c54b3c1f3bad2310084421df8d0158f1f60
abhikumar11 <abhishekkumar12verma@gmail.com>
2023-08-26 12:08:09 +0530

    initial commit

--- index.js
+++ index.js
@@ -2,6 +2,15 @@ const  express = require('express');
 const app = express();
 const port=3000;
 const {db}=require('./config/mongoose');
+const bodyparser = require('body-parser');
+const path=require('path');
+app.use(bodyparser.urlencoded({extended:false}));
+app.use(bodyparser.json());
+app.set('views',path.join(__dirname, 'views'));
+app.set('view engine','ejs');
+app.use(require('./routes'));
+
+
 app.listen(port,()=>{
     console.log(`listening on port ${port}`);
 });
